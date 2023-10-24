bf7cae500e04e6644b65df61b303c2a757880e0e
Deidei-Bear <1973930251@qq.com>
2023-10-24 11:48:08 +0800

    添加项目文件。

---
+++ test_10_24/test.c
@@ -0,0 +1,78 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+//int main()
+//{
+//	printf("好\n好\n\n休息一下吧\n\n");
+//	//printf是一个库函数 专门用来打印数据的，包含在stdio.h中
+//    //std 标准， i-input， o-output。标准输入输出头文件；
+//	
+//	printf("%zu\n", sizeof(char));
+//	printf("%zu\n", sizeof(short));
+//	printf("%zu\n", sizeof(int));
+//	printf("%zu\n", sizeof(long));
+//	printf("%zu\n", sizeof(long long));
+//	printf("%zu\n", sizeof(float));
+//	printf("%zu\n", sizeof(double));
+//	return 0;
+//	//约定：程序运行正常返回0，异常返回非0；
+//}
+////main函数是程序的入口，必须有且唯一。
+//
+
+//int main()
+//{
+//	int a = 22;
+//	double price = 66.6;
+//
+//	return 0;
+//}
+
+
+
+////变量和常量
+//
+////变量分为：局部变量；全局变量。
+//
+//
+//int b = 9;//全局变量
+//
+//int main()
+//{
+//	short age = 20;//年龄
+//	int high = 180;//身高
+//	float weight = 65;//体重
+//	 //局部变量
+//
+//	return 0;
+//}
+//
+////若全局变量与局部变量名称相同，优先使用局部变量；
+
+
+
+
+////计算2个整数的和
+////scanf 是一个输入函数
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;//初始化
+//	//如果没有初始化，存放随机值。编译有警告。
+//	
+//
+//	//输入两个整数
+//	scanf("%d %d", &num1, &num2);
+//
+//	//，求和
+//	int sum = num1 + num2;
+//
+//	//，输出
+//	printf("%d\n", sum);
+//
+//	return 0;
+//}  
+
+
+
+//变量生命周期
